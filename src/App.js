8ba4b6e2d193af275a1075656f96b78a2ff749e4
srikanth4c <srikanthbinf@gmail.com>
2020-11-16 03:31:07 +0530

    navbar component added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './navBar/Header';
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Amazon clone</h1>
         <Switch>
-            <Route path='/login'>
+            <Route path='/account'>
                 <h1>login page</h1>
             </Route>
             <Route exact path='/checkout/:val'>
               <h2>checkout</h2>
             </Route>
             <Route exact path='/'>
-              <h2>home</h2>
+              <Header />
+              <h1>Home page</h1>
             </Route>
         </Switch>
       </div>
--- src/navBar/Header.jsx
+++ src/navBar/Header.jsx
@@ -1,16 +1,64 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import './Header.css'
 function Header() {
     return (
-        <div>
+        <nav className='header'>
             {/*
              side nav-bar
              logo
-             header 
+             searchbox(inputbox, searchlogo)
              3 links ( accound , orderr, try prime)
              cart icon
-            */}
-        </div>
+            */
+            }
+
+            {/* logo */}
+            <Link to='/'>
+                <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon_logo"/>
+            </Link>
+            {/* End of Logo */}
+            {/* searchBar */}
+            <div className="header__searchBox">
+                <input className='header__searchInput' />
+                <SearchIcon   className='header__serachIcon' />
+            </div>
+            {/* End of SearchBar */}
+            {/* 3 links --- navlinks */}
+            <div className="header__navBar">
+                {/* account link */}
+                <Link to='/account' className='header__link'>
+                    <div className="header__option">
+                        <span>Hello, Sign in</span>
+                        <span>Account</span>
+                    </div>
+                </Link>
+                {/* Order link */}
+                <Link to='/returns' className='header__link'>
+                    <div className="header__option">
+                        <span>Returns</span>
+                        <span>& Orders</span>
+                    </div>
+                </Link>
+                {/* try Prime link */}
+                <Link to='/try_prime' className='header__link'>
+                    <div className="header__option">
+                        <span>Try</span>
+                        <span>Prime</span>
+                    </div>
+                </Link>
+                {/* add to card link */}
+                <Link to='/cart' className='header__link'>
+                    <div className="header__optionCart">
+                        <ShoppingCartOutlinedIcon  fontSize='large'/>
+                        <span>10</span>
+                    </div>
+                </Link>
+            </div>
+            {/* End of navLinks */}
+        </nav>
     )
 }
 
