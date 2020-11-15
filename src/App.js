a1f804c26f08cb3c56b129acf7220e2a3c800afc
srikanth4c <srikanthbinf@gmail.com>
2020-11-16 01:42:47 +0530

    amazon clone app initialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Amazon clone</h1>
+        <Switch>
+            <Route path='/login'>
+                <h1>login page</h1>
+            </Route>
+            <Route exact path='/checkout/:val'>
+              <h2>checkout</h2>
+            </Route>
+            <Route exact path='/'>
+              <h2>home</h2>
+            </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/navBar/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            {/*
+             side nav-bar
+             logo
+             header 
+             3 links ( accound , orderr, try prime)
+             cart icon
+            */}
+        </div>
+    )
+}
+
+export default Header
